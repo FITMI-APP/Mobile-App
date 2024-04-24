6079b389043cef50dcf1706419a86afda8635346
BatoolSherif <batoolsheriff@gmail.com>
2024-04-24 21:48:11 +0200

    home woman and man button issue solved

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,17 +1,12 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:grad/screens/waredrobe/wardrobe.dart';
-//import 'package:grad/services/authenticate.dart';
-
+import 'package:animated_button_bar/animated_button_bar.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:hexcolor/hexcolor.dart';
-import '../../shared/constants.dart';
+import 'package:path/path.dart' as path;
 import '../../shared/Header.dart';
-import '../../widget/button_widget.dart';
 import '../../widget/navigation_drawer_widget.dart';
 import 'GenerateImageCard.dart';
-import 'package:animated_button_bar/animated_button_bar.dart';
-import 'package:path/path.dart' as path;
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -21,95 +16,118 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-   //final AuthService _auth = AuthService();
-   File? cloth;
-   File? person;
-   String category = '';
-   String gender = '';
-   // File? personImageName ; // Variable to store person image name
-   // File? clothImageName;// Variable to store cloth image name
-   String? _selectedCategory;
-   bool _isDropdownVisible = false;
-   @override
-   Widget build(BuildContext context) {
+  File? cloth;
+  File? person;
+  String gender = 'woman'; // Default selection
+  String category = 'upper';
 
-     final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-
-     return Scaffold(
-       backgroundColor: HexColor("#3F72AF"),
-       drawer: NavigationDrawerWidget(),
-       appBar: Header(),
-
-       body:
-       Column(
-         crossAxisAlignment: CrossAxisAlignment.center,
-
-         children: [
-           Column(
-             //mainAxisAlignment: MainAxisAlignment.center,
-             children: [
-
-               //You can populate it with different types of widgets like Icon
-               AnimatedButtonBar(
-                 radius: 32.0,
-                 padding: const EdgeInsets.all(10.0),
-                 backgroundColor: HexColor("#aec2e6"),
-                 foregroundColor: HexColor("#DBE2EF"),
-                 //elevation: 24,
-                 borderColor: Colors.white,
-                 borderWidth: 2,
-                 innerVerticalPadding: 14,
-                 children: [
-                   //if man remove dress
-                   ButtonBarEntry(
-                       onTap: ()  {setState(() {gender = 'woman';});
-                         print('woman tapped');},
-                       child: Text("woman",style: const TextStyle(fontWeight: FontWeight.bold))),
-                   ButtonBarEntry(
-                       onTap: ()  {setState(() {gender = 'man';});
-                         print('man tapped');},
-                       child: Text("man", style: const TextStyle(fontWeight: FontWeight.bold))),
-                 ],
-               ),
-               //inverted selection button bar
-               const SizedBox(height: 4),
-
-               AnimatedButtonBar(
-                 radius: 32.0,
-                 padding: const EdgeInsets.fromLTRB(10.0,10.0,10.0,20.0),
-                 //invertedSelection: true,
-                 backgroundColor: HexColor("#aec2e6"),
-                 foregroundColor: HexColor("#DBE2EF"),
-                 borderWidth: 2,
-                 innerVerticalPadding: 14,
-                 borderColor: Colors.white,
-                 children: [
-                   ButtonBarEntry(
-                     onTap: () {setState(() {category = 'upper';});
-                     print('upper tapped');
-                     },
-                     child: Text('upper'),
-                   ),
-                   ButtonBarEntry(
-                     onTap: () {setState(() {category = 'lower';});
-                     print('lower tapped');
-                     },
-                     child: Text('lower'),
-                   ),
-                   ButtonBarEntry(
-                     onTap: () {setState(() {category = 'dress';});
-                     print('dress tapped');
-                     },
-                     child: Text('dress'),
-                   ),
-                 ],
-               ),
-
-               // const SizedBox(width: 10),
-
-             ],
-           ),
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: HexColor("#3F72AF"),
+      drawer: NavigationDrawerWidget(),
+      appBar: Header(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          AnimatedButtonBar(
+            radius: 32.0,
+            padding: const EdgeInsets.all(10.0),
+            backgroundColor: HexColor("#aec2e6"),
+            foregroundColor: HexColor("#DBE2EF"),
+            borderColor: Colors.white,
+            borderWidth: 2,
+            innerVerticalPadding: 14,
+            children: [
+              ButtonBarEntry(
+                onTap: () {
+                  setState(() {
+                    gender = 'woman';
+                  });
+                  print('woman tapped');
+                },
+                child: Text(
+                  "woman",
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              ButtonBarEntry(
+                onTap: () {
+                  setState(() {
+                    gender = 'man';
+                    if (category == 'dress') {
+                      category = 'upper'; // Change category if 'dress' is selected when gender is switched to 'man'
+                    }
+                  });
+                  print('man tapped');
+                },
+                child: Text(
+                  "man",
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 4),
+          AnimatedButtonBar(
+            radius: 32.0,
+            padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 20.0),
+            backgroundColor: HexColor("#aec2e6"),
+            foregroundColor: HexColor("#DBE2EF"),
+            borderWidth: 2,
+            innerVerticalPadding: 14,
+            borderColor: Colors.white,
+            children: gender == 'woman'
+                ? [
+              ButtonBarEntry(
+                onTap: () {
+                  setState(() {
+                    category = 'upper';
+                  });
+                  print('upper tapped');
+                },
+                child: Text('upper'),
+              ),
+              ButtonBarEntry(
+                onTap: () {
+                  setState(() {
+                    category = 'lower';
+                  });
+                  print('lower tapped');
+                },
+                child: Text('lower'),
+              ),
+              ButtonBarEntry(
+                onTap: () {
+                  setState(() {
+                    category = 'dress';
+                  });
+                  print('dress tapped');
+                },
+                child: Text('dress'),
+              ),
+            ]
+                : [
+              ButtonBarEntry(
+                onTap: () {
+                  setState(() {
+                    category = 'upper';
+                  });
+                  print('upper tapped');
+                },
+                child: Text('upper'),
+              ),
+              ButtonBarEntry(
+                onTap: () {
+                  setState(() {
+                    category = 'lower';
+                  });
+                  print('lower tapped');
+                },
+                child: Text('lower'),
+              ),
+            ],
+          ),
           SizedBox(height: 10),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -137,10 +155,8 @@ class _HomeState extends State<Home> {
             ],
           ),
           const SizedBox(height: 20),
-          // Generate button & wardrobe
           Container(
             height: 200,
-            // Adjust the height according to your preference
             child: SizedBox(
               width: double.infinity,
               child: Padding(
@@ -153,21 +169,26 @@ class _HomeState extends State<Home> {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (context) => GenerateImageCard(gender: gender, category: category, personImageName: person, clothImageName: cloth),
+                              builder: (context) => GenerateImageCard(
+                                gender: gender,
+                                category: category,
+                                personImageName: person,
+                                clothImageName: cloth,
+                              ),
                             ),
                           );
                         } else {
-                          // Show a message or prevent the button action if images are not inserted
                           ScaffoldMessenger.of(context).showSnackBar(
                             SnackBar(
-                              content: Text('Please insert both person and cloth images.'),
+                              content: Text(
+                                  'Please insert both person and cloth images.'),
                             ),
                           );
                         }
                       },
                       style: ButtonStyle(
                         backgroundColor:
-                            MaterialStateProperty.all<Color>(HexColor("#DBE2EF")),
+                        MaterialStateProperty.all<Color>(HexColor("#DBE2EF")),
                       ),
                       child: const Text(
                         'Generate',
@@ -177,16 +198,16 @@ class _HomeState extends State<Home> {
                             fontWeight: FontWeight.bold),
                       ),
                     ),
-
                   ],
                 ),
               ),
             ),
           ),
-         ],
-       ),
-     );
-   }
+        ],
+      ),
+    );
+  }
+
 
   void getImage({required ImageSource source, required String type}) async {
     final file = await ImagePicker().pickImage(
