3d8a6e573f203d66828a219027372ae5d93154f0
BatoolSherif <batoolsheriff@gmail.com>
2024-05-31 03:13:12 +0300

    new home UI

--- lib/shared/Header.dart
+++ lib/shared/Header.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:grad/services/authenticate.dart';
 import '../../shared/constants.dart';
 import 'package:hexcolor/hexcolor.dart';
@@ -15,15 +16,16 @@ class Header extends StatelessWidget implements PreferredSizeWidget {
     final AuthService _auth = AuthService();
 
     return AppBar(
-      backgroundColor: Colors.white,
+     // backgroundColor: Colors.white,
+
       bottom: PreferredSize(
         preferredSize: Size.fromHeight(56), // Adjust the height as needed
         child: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
               colors: [
-                HexColor("#553e8c"),
-                HexColor("#5419d3"),
+                HexColor("#3f1a8d"),
+                HexColor("#4e24ae"),
               ],
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
@@ -31,7 +33,8 @@ class Header extends StatelessWidget implements PreferredSizeWidget {
           ),
           child: AppBar(
             backgroundColor: Colors.transparent, // Make the inner AppBar transparent
-            elevation: 0, // Set elevation to 0 to avoid double shadow
+            elevation: 0,
+            // Set elevation to 0 to avoid double shadow
             title: Row(
               children: [
                 Text(
